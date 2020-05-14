e19745365f37299f3b8469a77022f9ab7f81016e
Everson dos Santos de Menezes <everson-menezes@bol.com.br>
2020-05-14 15:51:44 -0300

    Implementando tabuleiro

---
+++ Xadrez/Tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao _posicao { get; set; }
+        public Cor _cor { get; protected set; }
+        public int _qtdMovimentos { get; protected set; }
+        public Tabuleiro _tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            _posicao = posicao;
+            _cor = cor;
+            _qtdMovimentos = 0;
+            _tabuleiro = tabuleiro;
+        }
+    }
+}
