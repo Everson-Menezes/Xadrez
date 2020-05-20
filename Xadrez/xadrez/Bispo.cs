7c58822fec3f9e1b216c55f1ef8d05a84b452334
Everson dos Santos de Menezes <everson-menezes@bol.com.br>
2020-05-20 17:27:11 -0300

    Correcao movimentos bispo

--- Xadrez/xadrez/Bispo.cs
+++ Xadrez/xadrez/Bispo.cs
@@ -91,59 +91,6 @@ namespace xadrez
             {
                 matriz[pos._linha, pos._coluna] = true;
             }
-            // acima
-            pos.setarValores(_posicao._linha - 1, _posicao._coluna);
-
-            while (_tabuleiro.posicaoPermitida(pos) && podeMovimentar(pos))
-            {
-                matriz[pos._linha, pos._coluna] = true;
-                if (_tabuleiro.peca(pos) != null && _tabuleiro.peca(pos)._cor != _cor)
-                {
-                    break;
-                }
-                pos._linha = pos._linha - 1;
-            }
-
-
-            // baixo
-            pos.setarValores(_posicao._linha + 1, _posicao._coluna);
-
-            while (_tabuleiro.posicaoPermitida(pos) && podeMovimentar(pos))
-            {
-                matriz[pos._linha, pos._coluna] = true;
-                if (_tabuleiro.peca(pos) != null && _tabuleiro.peca(pos)._cor != _cor)
-                {
-                    break;
-                }
-                pos._linha = pos._linha + 1;
-            }
-
-            // esquerda
-            pos.setarValores(_posicao._linha, _posicao._coluna - 1);
-
-            while (_tabuleiro.posicaoPermitida(pos) && podeMovimentar(pos))
-            {
-                matriz[pos._linha, pos._coluna] = true;
-                if (_tabuleiro.peca(pos) != null && _tabuleiro.peca(pos)._cor != _cor)
-                {
-                    break;
-                }
-                pos._coluna = pos._coluna - 1;
-            }
-
-
-            // direita
-            pos.setarValores(_posicao._linha, _posicao._coluna + 1);
-
-            while (_tabuleiro.posicaoPermitida(pos) && podeMovimentar(pos))
-            {
-                matriz[pos._linha, pos._coluna] = true;
-                if (_tabuleiro.peca(pos) != null && _tabuleiro.peca(pos)._cor != _cor)
-                {
-                    break;
-                }
-                pos._coluna = pos._coluna + 1;
-            }
             return matriz;
         }
 
